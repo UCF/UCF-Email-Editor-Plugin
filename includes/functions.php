e1aafa88115eb7e2e0670bcdd8e68f9df957508d
Jo Dickson <jogreybill@gmail.com>
2020-03-25 16:25:24 -0400

    Added strong tag filtering

--- includes/functions.php
+++ includes/functions.php
@@ -38,6 +38,21 @@ function get_table_close_markup() {
 }
 
 /**
+ * Returns <span> tag closing email markup
+ *
+ * @since 1.1.3
+ * @author Jo Dickson
+ * @return string table markup
+ */
+function get_span_close_markup() {
+	ob_start();
+	?>
+		</span>
+	<?php
+	return ob_get_clean();
+}
+
+/**
  * Returns h2 open email markup
  *
  * @since 1.0.0
@@ -51,6 +66,21 @@ function get_h2_open_markup() {
 			<tbody>
 				<tr>
 					<td class="montserratbold" style="width: 100%; font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: bold; padding: 0px 0px 16px 0px; margin: 0;">
+	<?php
+	return ob_get_clean();
+}
+
+/**
+ * Returns <strong> tag opening markup for emails
+ *
+ * @since 1.1.3
+ * @author Jo Dickson
+ * @return string table markup
+ */
+function get_strong_open_markup() {
+	ob_start();
+	?>
+		<span class="montserratbold" style="font-family: Helvetica, Arial, sans-serif; font-weight: bold;">
 	<?php
 	return ob_get_clean();
 }
@@ -81,6 +111,21 @@ function convert_p_tags( $content ) {
 function convert_h2_tags( $content ) {
 	$content = preg_replace('/<h2[^>]*>/', get_h2_open_markup(), $content);
 	$content = preg_replace('/<\/h2>/', get_table_close_markup(), $content);
+
+	return $content;
+}
+
+/**
+ * Convert <strong> tags to email-friendly markup
+ *
+ * @since 1.1.3
+ * @author Jo Dickson
+ * @param string $content Unfiltered email content
+ * @return string Filtered email content
+ */
+function convert_strong_tags( $content ) {
+	$content = preg_replace( '/<strong[^>]*>/', get_strong_open_markup(), $content );
+	$content = preg_replace( '/<\/strong>/', get_span_close_markup(), $content );
 
 	return $content;
 }
@@ -136,6 +181,7 @@ function convert_content_to_email_markup( $content ) {
 
 	$content = convert_p_tags( $content );
 	$content = convert_h2_tags( $content );
+	$content = convert_strong_tags( $content );
 	$content = convert_list_tags( $content, 'ul' );
 	$content = convert_list_tags( $content, 'ol' );
 	$content = convert_li_tags( $content );
