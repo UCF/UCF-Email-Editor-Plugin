005f51a4e44cd82cb4327243d53133e2450d5862
RJ Bruneel <rj.bruneel@ucf.edu>
2020-04-06 14:54:23 -0400

    Added missing td tag

--- templates/blank/footer.php
+++ templates/blank/footer.php
@@ -1,26 +1,25 @@
 
-                                          </td>
-                                      </tr>
-                                      <tr>
-                                          <td style="padding: 0;">
-                                              <?php echo get_email_footer(); ?>
-                                          </td>
-                                      </tr>
-                                    </tbody>
-                                  </table>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </td>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding: 0;">
+                                            <?php echo get_email_footer(); ?>
+                                        </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </tr>
     </tbody>
   </table>
